56c0b326080d5586fa23912aa3b2872fffd9bd9b
蜉蝣 <40737721+SunRedSoldier@users.noreply.github.com>
2020-07-02 04:23:10 +0800

    Add files via upload

--- js/model.js
+++ js/model.js
@@ -1,4 +1,4 @@
-function qkeyword() {
+function loadKey() {
     // remerror()
     $.ajax({
         type : "GET",
@@ -9,14 +9,13 @@ function qkeyword() {
         dataType : "json",
         success : function(data) {
             data = data['data'][0]
-            var str = '';
-            var tips = '修改关键词';
             if (data != null){
                 $.each(data, function(key,value) {
                     if(key != 'ID'){
                         $("#"+key).val(value);
                     }
                 });
+                $("#updateKeyModalLabel").text("当前关键词")
             }else{
                 $("#updateKeyModalLabel").text("当前没有关键词！");
             }
@@ -29,5 +28,72 @@ function qkeyword() {
 
 }
 
+function checkNull(t){
+    if(t==''){
+        return null
+    }else{
+        return t
+    }
+}
+function updateKey(){
+    var key1 = $.trim($("#key1").val());
+    var key2 = $.trim($("#key2").val());
+    var key3 = $.trim($("#key3").val());
+    var key4 = $.trim($("#key4").val());
+    key1 = checkNull(key1)
+    key2 = checkNull(key2)
+    key3 = checkNull(key3)
+    key4 = checkNull(key4)
+    var rule = $("#rule").val();
+    if (rule == ''){
+        rule = '0';
+    }
+    var da = {"key1":key1,"key2":key2,"key3":key3,"key4":key4,"rulecode":rule };
+    $.ajax({
+        type : "POST",
+        url : "http://120.79.187.63:5701/catoption/upkeyword",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify(da),
+        dataType : "json",
+        success : function(data) {
+            if(data['code']=='200'){
+                $("#updateKeyModalLabel").text("修改成功！")
+                hide();
+            }else{
+                $("#updateKeyModalLabel").text("修改失败！")
+            }
+        },
+        error : function(data) {
+            $("#updateKeyModalLabel").text("出现错误！")
+        }
+    });
+}
 
-$(document).ready(qkeyword());
+function hide(){
+    $('#updateKeyModal').modal('hide')
+}
+
+$('[name="status"]').bootstrapSwitch({
+    onText: "交集",  // 设置ON文本  
+    offText: "并集",  // 设置OFF文本
+    onColor: "success",  // 设置ON文本颜色     (info/success/warning/danger/primary)  
+    offColor: "info",  // 设置OFF文本颜色        (info/success/warning/danger/primary)  
+    size: "small",   // 设置控件大小,从小到大  (mini/small/normal/large)  
+    handleWidth: "28",//设置控件宽度
+    onSwitchChange: function (event, state) {
+        if (state == true) {
+            $('#rule').val('1');
+        } else {
+            $('#rule').val('0');
+        }
+    }
+
+})
+
+$('#updateKeyModal').on('show.bs.modal', function (e) {
+    $("#updateKeyModalLabel").text("正在获取当前关键词...")
+    loadKey();
+})
+
+
+
