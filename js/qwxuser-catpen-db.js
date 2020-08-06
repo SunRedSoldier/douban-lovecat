def6db3d533e42cec4fb4d5b471a2cef7acdb3e6
蜉蝣 <40737721+SunRedSoldier@users.noreply.github.com>
2020-08-06 18:44:27 +0800

    Update qwxuser-catpen-db.js

--- js/qwxuser-catpen-db.js
+++ js/qwxuser-catpen-db.js
@@ -28,7 +28,7 @@ function qDBuser() {
         success : function(data) {
             data = data['data']
             var str = '';
-            var tips = '爱猫生活WxPusher订阅者(DB)，在数据库中的用户才会收到推送'
+            var tips = '爱猫澡盆WxPusher订阅者(DB)，在数据库中的用户才会收到推送'
             if(data != null){
                 $.each(data, function(key,value) {
                     if(key==0){
